519c60c05c35bf54e77e19e83520f8b90160f900
zerdZ <apple@applematoMac-mini.local>
2014-03-21 14:51:55 +0800

    素材

---
+++ Source/MusicPro/MusicPro/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  MusicPro
+//
+//  Created by apple  on 14-3-21.
+//  Copyright (c) 2014年 zdzhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainViewController : UIViewController
+
+
+
+@end
---
+++ Source/MusicPro/MusicPro/MainViewController.m
@@ -0,0 +1,13 @@
+//
+//  MainViewController.m
+//  MusicPro
+//
+//  Created by apple  on 14-3-21.
+//  Copyright (c) 2014年 zdzhong. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+@end
